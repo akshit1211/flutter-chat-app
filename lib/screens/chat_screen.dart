88703974186137047f86caaea47d6c6c8b9d6e8e
akshit1211 <trehana12@gmail.com>
2024-02-15 12:14:29 +0530

    final functionality(scroll view)

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -30,34 +30,74 @@ class _ChatScreenState extends State<ChatScreen> {
                   context.navigateToScreen(LoginScreen(), isReplace: true);
                 });
               },
-              icon: const Icon(Icons.logout)),
+              icon: const Icon(
+                Icons.logout,
+              )),
         ],
       ),
-      body: Column(children: [
-        const Spacer(),
-        Container(
-          width: double.infinity,
-          height: 100,
-          color: Colors.blue,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 7.0, left: 12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: context.getWidth(percentage: 0.80),
-                  child: TextField(
-                    controller: messageController,
-                    decoration: const InputDecoration(
-                        fillColor: Colors.white, filled: true),
+      body: Stack(children: [
+        // to fetch messages from firebase and display them on screen with stream builder.
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('messages').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Failed to fetch messages.'),
+              );
+            } else if (snapshot.hasData) {
+              final messages = snapshot.data!.docs;
+              return Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: 120,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(messages[index]['message']),
+                        subtitle: Text(messages[index]['senderEmail']),
+                      );
+                    }),
+              );
+            } else {
+              return const Center(child: Text('No messages available'));
+            }
+          },
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.blue,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 7.0, left: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: context.getWidth(percentage: 0.80),
+                    child: TextField(
+                      controller: messageController,
+                      decoration: const InputDecoration(
+                          fillColor: Colors.white, filled: true),
+                    ),
                   ),
-                ),
-                IconButton(
-                    onPressed: () {
-                      sendMessage();
-                    },
-                    icon: const Icon(Icons.send))
-              ],
+                  IconButton(
+                      onPressed: () {
+                        sendMessage();
+                      },
+                      icon: const Icon(Icons.send))
+                ],
+              ),
             ),
           ),
         )
@@ -75,12 +115,19 @@ class _ChatScreenState extends State<ChatScreen> {
         'senderEmail': FirebaseAuth.instance.currentUser!.email,
         'timeStamp': DateTime.now().microsecondsSinceEpoch,
       };
-      FirebaseFirestore.instance
+      await FirebaseFirestore.instance
           .collection('messages')
           .add(message)
           .then((value) {
         messageController.clear();
       });
     }
+  }
+
+  void getMessages() {
+    FirebaseFirestore.instance
+        .collection('messages')
+        .snapshots()
+        .listen((event) {});
   }
 }
